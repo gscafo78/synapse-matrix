6903af725c295cd420eb207c5c2ba03526f83107
Giovanni Scafetta <giovanni.scafetta@gmail.com>
2025-03-04 14:47:09 +0000

    .

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-apt update -y
-apt install gettext-base -y
-export $(grep -v '^#' ./.env | xargs)
-envsubst < ./coturn/turnserver.template.conf > ./coturn/turnserver.conf
-envsubst < ./homeserver.template.yaml > ./homeserver.yaml
-envsubst < ./element-config.template.json > ./element-config.json
+if [ ! -f ./homeserver.yaml ]; then
+  docker compose run --rm synapse generate
+  echo "Now, update the .env file with the new data, then run this script once."
+else
+  apt update -y
+  apt install gettext-base -y
+  export $(grep -v '^#' ./.env | xargs)
+  envsubst < ./coturn/turnserver.template.conf > ./coturn/turnserver.conf
+  envsubst < ./homeserver.template.yaml > ./homeserver.yaml
+  envsubst < ./element-config.template.json > ./element-config.json
+fi
