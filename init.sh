49346d8d9c367d8ff256361cc54643209200b041
Giovanni Scafetta <giovanni.scafetta@gmail.com>
2025-03-04 16:09:37 +0000

    .

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f ./homeserver.yaml ]; then
+if [ ! -f ./synapse-data/homeserver.yaml ]; then
   docker compose run --rm synapse generate
   echo "Now, update the .env file with the new data, then run this script once."
 else
@@ -8,6 +8,6 @@ else
   apt install gettext-base -y
   export $(grep -v '^#' ./.env | xargs)
   envsubst < ./coturn/turnserver.template.conf > ./coturn/turnserver.conf
-  envsubst < ./homeserver.template.yaml > ./homeserver.yaml
+  envsubst < ./homeserver.template.yaml > ./synapse-data/homeserver.yaml
   envsubst < ./element-config.template.json > ./element-config.json
 fi
